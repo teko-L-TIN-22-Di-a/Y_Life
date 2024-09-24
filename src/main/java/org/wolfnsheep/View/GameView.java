43b4a30771b5a453592d014fd745f87acc0ae244
Yamina <yamina.siegrist@gmail.com>
2024-09-24 10:21:59 +0200

    add javadoc

--- src/main/java/org/wolfnsheep/View/GameView.java
+++ src/main/java/org/wolfnsheep/View/GameView.java
@@ -12,7 +12,7 @@ public class GameView extends JPanel {
     private GameModel model;
 
     /**
-     * Konstruktor für die GameView, der das Modell setzt und die Grösse der Ansicht festlegt.
+     * Setzt Modell und die Grösse der Ansicht festlegt.
      *
      * @param model das Spielfeld
      */
